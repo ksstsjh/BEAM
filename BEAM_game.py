2fcbe189dd0b5cc3e1943e26e85916ebee6738b7
Kalina <44375307+mederq@users.noreply.github.com>
2021-06-03 22:34:49 +0200

    Poziom 1 – basics of the basics
    Co dziala:
    * gracz jest czerwonym prostokatem, ktory po zderzeniu z rozowymi drzwiami zmienia kolor na zielony
    * granice pokoju dzialaja
    
    TO-DO:
    * przeciwnicy
    * podmiana grafiki
    * level space invaders i quiz (w osobnych gameloopach)

--- BEAM_game.py
+++ BEAM_game.py
@@ -1,4 +1,4 @@
-import pygame, sys, config
+import pygame, sys
 
 # init i inne pierdy dla formalności
 pygame.init()
@@ -11,45 +11,63 @@ screen = pygame.display.set_mode((640,480))
 background = pygame.image.load('background.png')
 
 
-# player
+### basic ustawienia dla postaci gracza
 
-player_image = pygame.image.load('player_img.png')
 player_width = 40
 player_height = 50
 playerX = 300 # spawnuje się przy wejściu na osi X
 playerY = 400 # spawnuje się przy wejściu na osi Y
-player_velocity = 8
+player_velocity = 10
 
 
 
+### basic ustawienia dla drzwi 
 
-# game loop
+door_width = 20
+door_height = 100
+door_X = 10
+door_Y = 200
+
+
+
+### game loop
 
 run = True
 while run:
     pygame.time.delay(70)
 
-    for event in pygame.event.get():
+    for event in pygame.event.get():        # żeby program prawidłowo się zamykał po naciśnięcieu magicznego czerwonego guziczka
         if event.type == pygame.QUIT:
             run = False
-
+    
+    ######
+    ### sterowańsko 
+    ######
+    
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and playerX > player_velocity + 25:
+    if keys[pygame.K_LEFT] and playerX > player_velocity + 10: # fragmenty "and playerX > player_velocity + 10:" jest dla ustalenia granic pokoju
         playerX -= player_velocity
-    # if keys[pygame.K_LEFT] and playerX < 620 - player_velocity - player_width:
-    #     playerX -= player_velocity
-
-    if keys[pygame.K_RIGHT] and playerX < 620 - player_velocity - player_width:
+    if keys[pygame.K_RIGHT] and playerX < 600 - player_velocity - player_width:
         playerX += player_velocity
-    if keys[pygame.K_UP] and playerY > player_velocity + 25:
+    if keys[pygame.K_UP] and playerY > player_velocity + 10:
         playerY -= player_velocity
-    if keys[pygame.K_DOWN] and playerY < 460 - player_height - player_velocity:
+    if keys[pygame.K_DOWN] and playerY < 440 - player_height - player_velocity:
         playerY += player_velocity
 
 
     screen.blit(background, (0,0)) # dzięki temu nie "rysujemy" naszym ruchem,, tylko gracz chodzi bez zostawiania śladu (wystarczy zakomentować tę linijkę żeby zobaczyć o co cho)
-    pygame.draw.rect(screen, (255, 0, 0), (playerX, playerY, player_width, player_height))
+    player = pygame.draw.rect(screen, (255, 0, 0), (playerX, playerY, player_width, player_height)) # narysowanie gracza-prostokata
+    door = pygame.draw.rect(screen, (255, 200, 255), (door_X, door_Y, door_width, door_height)) # narysowanie drzwi
+
+# kolizja!!! teraz po zderzeniu z różowymi drzwiami gracz zmienia kolor na zielony, ale w domyśle w tym momencie kończy się obecny loop i zaczyna space invaders
+
+    collide = player.colliderect(door)
+    if collide:
+        player = pygame.draw.rect(screen, (0, 255, 0), (playerX, playerY, player_width, player_height))
+
+
+
     pygame.display.update()
 
 
